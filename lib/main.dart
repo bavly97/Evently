866c0f7b4e3f3739040a29bdd61d33af25940eb5
bavly maher <bmaziz97@gmail.com>
2025-01-25 17:44:00 +0200

    initialize project

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Evently());
+}
+
+class Evently extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
